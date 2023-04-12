ce2a5c1adedc5f8f5a8150f4a6ea149bc41e3c11
Park, Yunha <hadbsgk@gmail.com>
2023-04-12 23:36:07 +0900

    Setting: contentlayer 설치 및 기본 설정, gitignore에서 자동생성 폴더 제외처리(Resolved: #5)

---
+++ contentlayer.config.js
@@ -0,0 +1,47 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+
+export const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: `**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      description: 'The title of the post',
+      required: true,
+    },
+    publishedAt: {
+      type: 'date',
+      description: 'The date of the post',
+      required: true,
+    },
+    summary: {
+      type: 'string',
+      require: true,
+    },
+    image: {
+      type: 'string',
+    }
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.flattenedPath,
+    },
+    structuredData: {
+      type: 'object',
+      resolve: (doc) => ({
+        '@context': 'https://schema.org',
+        '@type': 'BlogPosting',
+        headline: doc.title,
+        datePublished: doc.publishedAt,
+        dateModified: doc.publishedAt,
+      }),
+    },
+  },
+}))
+
+export default makeSource({
+  contentDirPath: 'content',
+  documentTypes: [Blog],
+})
