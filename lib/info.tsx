b5d2212b952a6bcfd306c3dea94f962e5f2c2750
Park, Yunha <hadbsgk@gmail.com>
2023-04-15 17:19:07 +0900

    메인 페이지 작성, 세부 정보는 info.tsx 에서 별도 관리(Resolved: #1)

---
+++ lib/info.tsx
@@ -0,0 +1,12 @@
+export const name = "박 윤하";
+export const about = () => {
+  return (
+    <>
+      안녕하세요 블로그 주인입니다. 금융권에서 일하구요 개발하는거 좋아합니다.
+    </>
+  );
+};
+
+export const bio = () => {
+  return <>쉴때는 유튜브나 롤 하구요. 최근에 마스터 찍은적 있습니다.</>;
+};
