28c785f255e50d4cae575672f341d8c022691036
Park, Yunha <hadbsgk@gmail.com>
2023-04-15 17:21:33 +0900

    config: contentlayer 블로그 다큐먼트 타입 수정(카테고리 추가, 날짜 타입 변경 등) (#5)

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,47 +1,51 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
 export const Blog = defineDocumentType(() => ({
-  name: 'Blog',
+  name: "Blog",
   filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  contentType: "mdx",
   fields: {
     title: {
-      type: 'string',
-      description: 'The title of the post',
+      type: "string",
+      description: "The title of the post",
       required: true,
     },
     publishedAt: {
-      type: 'date',
-      description: 'The date of the post',
+      type: "string",
+      description: "The date of the post",
       required: true,
     },
+    category: {
+      type: "string",
+      require: true,
+    },
     summary: {
-      type: 'string',
+      type: "string",
       require: true,
     },
     image: {
-      type: 'string',
-    }
+      type: "string",
+    },
   },
   computedFields: {
     slug: {
-      type: 'string',
+      type: "string",
       resolve: (doc) => doc._raw.flattenedPath,
     },
     structuredData: {
-      type: 'object',
+      type: "object",
       resolve: (doc) => ({
-        '@context': 'https://schema.org',
-        '@type': 'BlogPosting',
+        "@context": "https://schema.org",
+        "@type": "BlogPosting",
         headline: doc.title,
         datePublished: doc.publishedAt,
         dateModified: doc.publishedAt,
       }),
     },
   },
-}))
+}));
 
 export default makeSource({
-  contentDirPath: 'content',
+  contentDirPath: "content",
   documentTypes: [Blog],
-})
+});
