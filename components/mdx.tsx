e91a87d29b5904ea53ac46625742766ffcfce9cf
Park, Yunha <hadbsgk@gmail.com>
2023-04-16 22:20:05 +0900

    Component: Aside 컴포넌트를 추가하여 MDX에서 사용할 수 있도록 적용(#9)

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -4,12 +4,27 @@ interface MdxProps {
   code: string;
 }
 
+function Aside(props) {
+  return (
+    <div>
+      <div className="mx-2 mb-2 h-[0.1em] bg-blue-600" />
+      <div className={`mx-4 text-blue-600`}>
+        <span className={`mr-3`}>⭐</span>️ {props.children}
+      </div>
+    </div>
+  );
+}
+
+const components = {
+  Aside,
+};
+
 export function Mdx({ code }: MdxProps) {
   const Component = useMDXComponent(code);
 
   return (
     <article className={`prose prose-neutral`}>
-      <Component />
+      <Component components={{ ...components }} />
     </article>
   );
 }
