622e904804c6d85ff90e9ee2c368a8e51ed63f04
Park, Yunha <hadbsgk@gmail.com>
2023-04-16 22:00:23 +0900

    fix: 블로그 세부 내용으로 이동하도록 링크 추가(#2)

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { allBlogs } from "contentlayer/generated";
+import Link from "next/link";
 
 export default async function BlogPage() {
   return (
@@ -37,36 +38,38 @@ export default async function BlogPage() {
 
 function Tile({ article }) {
   return (
-    <div className={`@container`}>
-      <div
-        className={`group flex h-full flex-col overflow-hidden rounded-2xl @[692px]:flex-row`}
-      >
-        {/* TODO: next/image 적용하기 */}
-        {/* shrink-0 is Silver Bullet!! */}
+    <Link href={`/blog/${article.slug}`}>
+      <div className={`@container`}>
         <div
-          className={`shrink-0 overflow-hidden @[692px]:w-[453px] @[980px]:w-[643px]`}
+          className={`group flex h-full flex-col overflow-hidden rounded-2xl @[692px]:flex-row`}
         >
-          <img
-            src={article.image}
-            className={`aspect-[1.77] h-auto w-full max-w-full object-cover transition-transform duration-300 group-hover:scale-105`}
-          />
-        </div>
-        <div className={`flex grow flex-col justify-between bg-white p-6`}>
-          <div className={`space-y-[4px] @[474px]:space-y-[8px]`}>
-            <p className={`text-[12px] font-bold text-gray-600`}>
-              {article.category}
-            </p>
-            <p
-              className={`mt-2 text-[19px] font-semibold @[305px]:leading-6 @[474px]:text-[24px] @[474px]:leading-8 @[692px]:text-[21px] @[980px]:text-[32px] @[980px]:font-bold @[980px]:leading-10`}
-            >
-              {article.title}
+          {/* TODO: next/image 적용하기 */}
+          {/* shrink-0 is Silver Bullet!! */}
+          <div
+            className={`shrink-0 overflow-hidden @[692px]:w-[453px] @[980px]:w-[643px]`}
+          >
+            <img
+              src={article.image}
+              className={`aspect-[1.77] h-auto w-full max-w-full object-cover transition-transform duration-300 group-hover:scale-105`}
+            />
+          </div>
+          <div className={`flex grow flex-col justify-between bg-white p-6`}>
+            <div className={`space-y-[4px] @[474px]:space-y-[8px]`}>
+              <p className={`text-[12px] font-bold text-gray-600`}>
+                {article.category}
+              </p>
+              <p
+                className={`mt-2 text-[19px] font-semibold @[305px]:leading-6 @[474px]:text-[24px] @[474px]:leading-8 @[692px]:text-[21px] @[980px]:text-[32px] @[980px]:font-bold @[980px]:leading-10`}
+              >
+                {article.title}
+              </p>
+            </div>
+            <p className={`mt-2 text-[14px] font-medium text-gray-500`}>
+              {article.publishedAt}
             </p>
           </div>
-          <p className={`mt-2 text-[14px] font-medium text-gray-500`}>
-            {article.publishedAt}
-          </p>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
