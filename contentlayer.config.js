b9795835fa7df850322708fdd4081a8c4c77e682
Park, Yunha <hadbsgk@gmail.com>
2023-04-18 23:46:03 +0900

    Style: 블로그 글 기본 스타일 적용(#17)
    - mdx 샘플 페이지에 예제 추가
    - markdown 관련 패키지 설치
    - contentlayer에 적용
    - 기본 css 추가

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,4 +1,8 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export const Blog = defineDocumentType(() => ({
   name: "Blog",
@@ -40,6 +44,13 @@ export const Blog = defineDocumentType(() => ({
         headline: doc.title,
         datePublished: doc.publishedAt,
         dateModified: doc.publishedAt,
+        description: doc.summary,
+        image: `https://llighter.io${doc.image}`,
+        url: `https://llighter.io/blog/${doc._raw.flattenedPath}`,
+        author: {
+          "@type": "Person",
+          name: "Park Yunha",
+        },
       }),
     },
   },
@@ -48,4 +59,37 @@ export const Blog = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: "content",
   documentTypes: [Blog],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: "one-dark-pro",
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }];
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted");
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word-highlighted"];
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
+  },
 });
