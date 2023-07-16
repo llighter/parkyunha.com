1c287710fbc5b375ade7bad4434b485441738d5a
Park, Yunha <hadbsgk@gmail.com>
2023-07-16 16:12:45 +0900

    로고 만들기(SVG) Fixes #48

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ function Logo() {
 
   return (
     <Link href={`/`} aria-label={`Park Yunha`}>
-      <img className={`h-24 w-28`} src={`/images/logo.svg`} />
+      <img className={`h-20 w-32`} src={`/images/logo.svg`} />
     </Link>
   );
 }
@@ -15,16 +15,26 @@ export default function Footer() {
   return (
     // <div className={styles.myfoot}>
     <footer className="mx-auto w-full tablet:w-full">
-      <div className="px-5 mx-auto laptop:max-w-screen-laptop_inner laptop:p-0">
+      <div className="mx-auto px-5 laptop:max-w-screen-laptop_inner laptop:p-0">
         <div>
           <Logo />
-          <ul className={`${styles.myfoot} text-sm font-semibold tablet:flex laptop:flex`}>
-            <li><a href="/">home</a></li>
-            <li><a href="/about">about</a></li>
-            <li><a href="/blog">blog</a></li>
+          <ul
+            className={`${styles.myfoot} text-sm font-semibold tablet:flex laptop:flex`}
+          >
+            <li>
+              <a href="/">home</a>
+            </li>
+            <li>
+              <a href="/about">about</a>
+            </li>
+            <li>
+              <a href="/blog">blog</a>
+            </li>
           </ul>
         </div>
-        <div className="text-sm text-gray-500 py-4 pb-5">Copyright &copy; 2023 YUNHA INDUSTRIES</div>
+        <div className="py-4 pb-5 text-sm text-gray-500">
+          Copyright &copy; 2023 YUNHA INDUSTRIES
+        </div>
       </div>
     </footer>
     // </div>
