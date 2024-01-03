b549b2dde0819244badc26eda89b755816c050da
Park, Yunha <hadbsgk@gmail.com>
2024-01-04 07:04:10 +0900

    홈페이지 내용 업데이트 및 사진 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,120 @@
-import { about, bio, name } from "@/lib/info";
+import Image from "next/image";
+import Link from "next/link";
+import hi_five from "public/images/home/hi-five.jpeg";
+import han_river from "public/images/home/han-river.jpeg";
+import eclipse from "public/images/home/eclipse.jpeg";
+import osaka_castle from "public/images/home/osaka-castle.jpeg";
+import seoul from "public/images/home/seoul.jpeg";
+import nanba_city_view from "public/images/home/nanba-city-view.jpeg";
+
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="inline-flex items-center rounded border border-neutral-200 bg-neutral-50 p-1 text-sm leading-4 text-neutral-900 no-underline"
+      // TODO: 다크모드 지원(dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-100)
+    />
+  );
+}
 
 export default function Home() {
   return (
     <section className={`mt-0 overflow-hidden pb-10 tablet:pb-16 laptop:pb-20`}>
       <div
-        className={`mx-auto mt-8 w-87.5 max-w-[366px] tablet:w-full tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
+        className={`mx-auto w-87.5 max-w-[366px] tablet:max-w-[576px] laptop:max-w-[653px]`}
       >
         <div className={`@container`}>
           <h2
-            className={`mb-8 text-[24px] font-bold @[692px]:text-[28px] @[980px]:text-[32px]`}
+            className={`my-8 text-[24px] font-bold @[576px]:text-[28px] @[653px]:text-[32px]`}
           >
-            &nbsp;박 윤하
+            &nbsp;안녕하세요, <b>박윤하</b>입니다.
           </h2>
         </div>
-        <p>{about()}</p>
-        <p>{bio()}</p>
+        <div className="prose prose-neutral">
+          <p>
+            {`안녕하세요. `}
+            <span className="not-prose">
+              <Badge href="https://www.parkyunha.com">parkyunha.com</Badge>
+            </span>
+            {` 블로그를 운영하고 있는 `}
+            <b>박윤하</b>
+            {`입니다. 금융권에서 비대면 업무인 모바일 앱과 홈페이지 등
+          채널계를 개발하는 `}
+            <Link href="/work">업무</Link>
+            {`를 합니다.`}
+          </p>
+        </div>
+        <div className="my-8 columns-2 gap-4 laptop:columns-3">
+          <div className="relative mb-4 h-40">
+            <Image
+              alt="Me and my younger brother at the Han River in Seoul, South Korea"
+              src={han_river}
+              fill
+              sizes="(max-width: 768px) 213px, 33vw"
+              priority
+              className="rounded-lg object-cover"
+            />
+          </div>
+          <div className="relative mb-4 h-80 laptop:mb-0">
+            <Image
+              alt="Me at Osaka Castle in Osaka, Japan"
+              src={osaka_castle}
+              fill
+              sizes="(max-width: 768px) 213px, 33vw"
+              priority
+              className="rounded-lg object-cover laptop:object-center"
+            />
+          </div>
+          <div className="relative h-40 laptop:mb-4 laptop:h-80">
+            <Image
+              alt="high five with the Glico sign"
+              src={hi_five}
+              fill
+              sizes="(max-width: 768px) 213px, 33vw"
+              priority
+              className="rounded-lg object-cover object-top laptop:object-center"
+            />
+          </div>
+          <div className="relative mb-4 h-40 laptop:mb-0">
+            <Image
+              alt="A picture I drew characterizing Eclipse and Java"
+              src={eclipse}
+              fill
+              sizes="(max-width: 768px) 213px, 33vw"
+              priority
+              className="rounded-lg object-cover"
+            />
+          </div>
+          <div className="relative mb-4 h-40">
+            <Image
+              alt="Seoul seen from above an airplane"
+              src={seoul}
+              fill
+              sizes="(max-width: 768px) 213px, 33vw"
+              priority
+              className="rounded-lg object-cover"
+            />
+          </div>
+          <div className="relative h-80">
+            <Image
+              alt="City view from Hotel Monterey Grasmere Osaka"
+              src={nanba_city_view}
+              fill
+              sizes="(min-width: 768px) 213px, 33vw"
+              priority
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
+        <div className="prose prose-neutral">
+          <p>
+            {`최근에는 Next.js를 사용해서 블로그를 만들고 있고 자바를 주로
+          개발합니다. 저는 스스로 만들어낸 결과를 다듬어서 여러 사람과 공유하는
+          것을 좋아합니다. 글이나 영상 혹은 나만의 프로덕트를 꾸준히 만들어
+          가는것이 목표입니다.`}
+          </p>
+        </div>
       </div>
     </section>
   );
