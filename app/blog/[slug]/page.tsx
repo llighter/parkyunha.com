7aebbee9b2de4ec9bb3739de51b2ab0a47bf7b1f
Park, Yunha <hadbsgk@gmail.com>
2024-12-07 20:36:04 +0900

    chore: prettier

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import { getBlogPosts } from 'app/db/blog';
+import { getBlogPosts } from "app/db/blog";
 import { notFound } from "next/navigation";
 // TODO: components 위치 옮기기
-import { CustomMDX } from 'components/mdx';
+import { CustomMDX } from "components/mdx";
 import Link from "next/link";
 import MailButton from "@/components/mailButton";
 import CopyLinkButton from "@/components/copyLinkButton";
@@ -61,14 +61,14 @@ export default function Blog({ params }) {
   }
 
   return (
-    <section className='mt-0 overflow-hidden'>
+    <section className="mt-0 overflow-hidden">
       <script
         type="application/ld+json"
         suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'BlogPosting',
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
             headline: post.metadata.title,
             datePublished: post.metadata.publishedAt,
             dateModified: post.metadata.publishedAt,
@@ -78,113 +78,105 @@ export default function Blog({ params }) {
               : `https://www.parkyunha.com/og?title=${post.metadata.title}`,
             url: `https://www.parkyunha.com/blog/${post.slug}`,
             author: {
-              '@type': 'Person',
-              name: 'Park Yunha',
+              "@type": "Person",
+              name: "Park Yunha",
             },
           }),
         }}
       />
-        <article id="article" className={`pt-10`}>
-          <div id="article-header">
-            <div
-              id="category"
-              className={`mx-auto mb-[20px] max-w-[414px] tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
-            >
-              <div
-                id="category-eyebrow"
-                className={`mx-auto w-87.5 text-xs font-bold text-gray-500 tablet:w-[576px] laptop:w-[653px]`}
-              >
-                {post.metadata.category}
-              </div>
-              <div
-                id="date-eyebrow"
-                className={`mx-auto mt-[4px] w-87.5 text-sm font-semibold text-gray-500 tablet:w-[576px] laptop:w-[653px]`}
-              >
-                {formatDate(post.metadata.publishedAt)}
-              </div>
-            </div>
-            <div
-              id="page-title"
-              className={`mx-auto max-w-[414px] tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
-            >
-              <div
-                className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}
-              >
-                <h1
-                  className={`table:text-[42px] break-keep text-[32px] font-extrabold leading-[1.21875] tablet:leading-[1.2] laptop:text-[48px] laptop:leading-[1.1875]`}
-                >
-                  {post.metadata.title}
-                </h1>
-              </div>
-            </div>
-            <div
-              id="page-summary"
-              className={`mx-auto mt-4 max-w-[414px] leading-[1.2858] tablet:max-w-screen-tablet_inner tablet:leading-[1.29167] laptop:mt-5 laptop:max-w-screen-laptop_inner laptop:leading-[1.19048]`}
-            >
-              <div
-                className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}
-              >
-                <div
-                  className={`break-keep text-[21px] font-bold laptop:text-[24px]`}
-                >
-                  {post.metadata.summary}
-                </div>
-              </div>
-            </div>
-            <div
-              id="sharesheet"
-              className={`mx-auto mt-[20px] max-w-[414px] tablet:max-w-screen-tablet_inner laptop:mt-6 laptop:max-w-screen-laptop_inner`}
-            >
-              <div
-                className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}
-              >
-                <ul className={`flex justify-start space-x-4 align-middle`}>
-                  <li className={`mt-3`}>
-                    <MailButton post={post} />
-                  </li>
-                  <li className={`mt-3`}>
-                    <CopyLinkButton />
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <figure className={`my-8 tablet:my-9 laptop:my-11`}>
-            <div
-              className={`relative mx-auto aspect-[1.5] max-w-[414px] tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
-            >
-              <Image
-                src={post.metadata.image}
-                alt={post.metadata.imageDescription}
-                fill={true}
-                priority={true}
-                className={`rounded-none object-cover phone:rounded-xl`}
-              />
-            </div>
-            <div
-              className={`mx-auto mt-[12px] max-w-[414px] leading-[1.2858] tablet:max-w-screen-tablet_inner tablet:leading-[1.29167] laptop:mt-4 laptop:max-w-screen-laptop_inner laptop:leading-[1.19048]`}
-            >
-              <div
-                className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}
-              >
-                <div className={`break-keep text-xs font-bold text-gray-500`}>
-                  {post.metadata.imageDescription}
-                </div>
-              </div>
-            </div>
-          </figure>
-
+      <article id="article" className={`pt-10`}>
+        <div id="article-header">
           <div
-            className={`mx-auto mb-11 mt-8 w-87.5 max-w-[362px] tablet:w-full tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
+            id="category"
+            className={`mx-auto mb-[20px] max-w-[414px] tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
           >
-            <article className="prose prose-quoteless prose-neutral mx-auto">
-              <CustomMDX source={post.content} />
-            </article>
+            <div
+              id="category-eyebrow"
+              className={`mx-auto w-87.5 text-xs font-bold text-gray-500 tablet:w-[576px] laptop:w-[653px]`}
+            >
+              {post.metadata.category}
+            </div>
+            <div
+              id="date-eyebrow"
+              className={`mx-auto mt-[4px] w-87.5 text-sm font-semibold text-gray-500 tablet:w-[576px] laptop:w-[653px]`}
+            >
+              {formatDate(post.metadata.publishedAt)}
+            </div>
           </div>
+          <div
+            id="page-title"
+            className={`mx-auto max-w-[414px] tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
+          >
+            <div className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}>
+              <h1
+                className={`table:text-[42px] break-keep text-[32px] font-extrabold leading-[1.21875] tablet:leading-[1.2] laptop:text-[48px] laptop:leading-[1.1875]`}
+              >
+                {post.metadata.title}
+              </h1>
+            </div>
+          </div>
+          <div
+            id="page-summary"
+            className={`mx-auto mt-4 max-w-[414px] leading-[1.2858] tablet:max-w-screen-tablet_inner tablet:leading-[1.29167] laptop:mt-5 laptop:max-w-screen-laptop_inner laptop:leading-[1.19048]`}
+          >
+            <div className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}>
+              <div
+                className={`break-keep text-[21px] font-bold laptop:text-[24px]`}
+              >
+                {post.metadata.summary}
+              </div>
+            </div>
+          </div>
+          <div
+            id="sharesheet"
+            className={`mx-auto mt-[20px] max-w-[414px] tablet:max-w-screen-tablet_inner laptop:mt-6 laptop:max-w-screen-laptop_inner`}
+          >
+            <div className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}>
+              <ul className={`flex justify-start space-x-4 align-middle`}>
+                <li className={`mt-3`}>
+                  <MailButton post={post} />
+                </li>
+                <li className={`mt-3`}>
+                  <CopyLinkButton />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <figure className={`my-8 tablet:my-9 laptop:my-11`}>
+          <div
+            className={`relative mx-auto aspect-[1.5] max-w-[414px] tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
+          >
+            <Image
+              src={post.metadata.image}
+              alt={post.metadata.imageDescription}
+              fill={true}
+              priority={true}
+              className={`rounded-none object-cover phone:rounded-xl`}
+            />
+          </div>
+          <div
+            className={`mx-auto mt-[12px] max-w-[414px] leading-[1.2858] tablet:max-w-screen-tablet_inner tablet:leading-[1.29167] laptop:mt-4 laptop:max-w-screen-laptop_inner laptop:leading-[1.19048]`}
+          >
+            <div className={`mx-auto w-87.5 tablet:w-[576px] laptop:w-[653px]`}>
+              <div className={`break-keep text-xs font-bold text-gray-500`}>
+                {post.metadata.imageDescription}
+              </div>
+            </div>
+          </div>
+        </figure>
 
-          {/* TODO: 조건을 처리하는 더 좋은 방법이 있는지 확인 필요 */}
-          {/* TODO: relatedArticles 사용하는 방식으로 변경 필요 */}
-          {/* {relatedArticles.length > 0 && (
+        <div
+          className={`mx-auto mb-11 mt-8 w-87.5 max-w-[362px] tablet:w-full tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
+        >
+          <article className="prose prose-quoteless prose-neutral mx-auto">
+            <CustomMDX source={post.content} />
+          </article>
+        </div>
+
+        {/* TODO: 조건을 처리하는 더 좋은 방법이 있는지 확인 필요 */}
+        {/* TODO: relatedArticles 사용하는 방식으로 변경 필요 */}
+        {/* {relatedArticles.length > 0 && (
             <div
               className={`mx-auto mb-11 mt-8 w-87.5 max-w-[362px] tablet:w-full tablet:max-w-screen-tablet_inner laptop:max-w-screen-laptop_inner`}
             >
@@ -202,30 +194,30 @@ export default function Blog({ params }) {
               </div>
             </div>
           )} */}
-        </article>
-        <aside className={`mt-20 flex justify-center bg-gray-50`}>
-          <Link href={`/blog`}>
-            <div
-              className={`mx-auto my-[44px] flex w-87.5 flex-col items-center tablet:my-[48px] tablet:w-[576px] laptop:my-[52px] laptop:w-[653px]`}
+      </article>
+      <aside className={`mt-20 flex justify-center bg-gray-50`}>
+        <Link href={`/blog`}>
+          <div
+            className={`mx-auto my-[44px] flex w-87.5 flex-col items-center tablet:my-[48px] tablet:w-[576px] laptop:my-[52px] laptop:w-[653px]`}
+          >
+            <figure
+              className={`h-[30px] w-[150px] bg-[url('/images/logo.svg')] bg-cover`}
             >
-              <figure
-                className={`h-[30px] w-[150px] bg-[url('/images/logo.svg')] bg-cover`}
-              >
-                <figcaption className={`hidden`}>logo</figcaption>
-              </figure>
-              <p
-                className={`mt-3 text-[17px] font-extrabold text-gray-500 laptop:text-[19px]`}
-              >
-                최신 글 및 업데이트
-              </p>
-              <p
-                className={`mt-[14px] rounded-3xl bg-gray-200 px-4 py-2 text-center text-[14px] font-bold laptop:mt-[18px]`}
-              >
-                더보기
-              </p>
-            </div>
-          </Link>
-        </aside>
+              <figcaption className={`hidden`}>logo</figcaption>
+            </figure>
+            <p
+              className={`mt-3 text-[17px] font-extrabold text-gray-500 laptop:text-[19px]`}
+            >
+              최신 글 및 업데이트
+            </p>
+            <p
+              className={`mt-[14px] rounded-3xl bg-gray-200 px-4 py-2 text-center text-[14px] font-bold laptop:mt-[18px]`}
+            >
+              더보기
+            </p>
+          </div>
+        </Link>
+      </aside>
     </section>
   );
 }
